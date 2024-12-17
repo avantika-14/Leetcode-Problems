a4901a5f64a000ec60139a78d3635f6a04f7acc0
avantika-14 <100148210+avantika-14@users.noreply.github.com>
2024-12-17 12:57:23 +0530

    Time: 2 ms (33.78%), Space: 41.7 MB (10.16%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -2,27 +2,38 @@ class Solution {
     public int splitArray(int[] nums, int k) {
         int start = 0;
         int end = 0;
-        for(int i = 0; i < nums.length; i++){
-            start = Math.max(start, nums[i]); //at the end of the loop this will contatin max value of the array
+
+        for (int i = 0; i < nums.length; i++) {
+            start = Math.max(start, nums[i]); // in the end of the loop this will contain the max item of the array
             end += nums[i];
         }
-        while(start < end){
-            int mid = start + (end - start)/2;
-            int sum = 0; //the sum of elements in the specific subarray
-            int count = 1;
-            for(int num:nums){
-                if(sum + num > mid){
+
+        // binary search
+        while (start < end) {
+            // try for the middle as potential ans
+            int mid = start + (end - start) / 2;
+
+            // calculate how many pieces you can divide this in with this max sum
+            int sum = 0;
+            int pieces = 1;
+            for(int num : nums) {
+                if (sum + num > mid) {
+                    // you cannot add this in this subarray, make new one
+                    // say you add this num in new subarray, then sum = num
                     sum = num;
-                    count ++;
-                }
-                else
+                    pieces++;
+                    } else {
                     sum += num;
+                }
             }
-            if(count <= k)
-                end = mid;
-            else
+
+            if (pieces > k) {
                 start = mid + 1;
+            } else {
+                end = mid;
+            }
+
         }
-         return end;
+        return end; // here start == end
     }
 }
